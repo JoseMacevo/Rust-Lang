8b0e7aff650f46a2a19b45c7c667508c09fa3a71
Jose Manuel <devcount@outlook.es>
2021-06-14 09:58:43 +0200

    Final del curso

---
+++ result_enums/target/debug/result_enums.d
@@ -0,0 +1,1 @@
+/home/josemanuelacevogarcia/Documentos/Development/Rust-Lang/result_enums/target/debug/result_enums: /home/josemanuelacevogarcia/Documentos/Development/Rust-Lang/result_enums/src/main.rs
